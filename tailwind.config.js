02936a97c00667b0de578985314b998a93fd8e9e
DaChoko <Mailechokoe2@gmail.com>
2026-03-11 16:24:39 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'ui-sans-serif', 'system-ui', 'Segoe UI', 'Helvetica Neue', 'Arial', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+}
